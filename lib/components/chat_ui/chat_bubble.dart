f0d5d5c12f9b08e1fcc50d8317e359ef0678bdd0
Mazahir <Supermazahir@gmail.com>
2023-11-24 16:30:14 +0530

    added persistent chats

--- lib/components/chat_ui/chat_bubble.dart
+++ lib/components/chat_ui/chat_bubble.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 
 class ChatBubble extends StatelessWidget {
   const ChatBubble(
       {super.key,
       required this.text,
       required this.isUser,
-      required this.date});
+      required this.date,
+      required this.isLoading});
 
   final String text;
   final bool isUser;
   final DateTime date;
+  final bool isLoading;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+      padding: EdgeInsets.only(
+          top: 15, right: isUser ? 10 : 60, left: isUser ? 60 : 10),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment:
@@ -27,24 +31,39 @@ class ChatBubble extends StatelessWidget {
                 color: Colors.transparent,
               ),
               borderRadius: BorderRadius.circular(10),
-              color: isUser ? Colors.blue : Colors.orange,
+              color: isUser
+                  ? Theme.of(context).colorScheme.secondary
+                  : Theme.of(context).colorScheme.primaryContainer,
             ),
-            child: Text(
-              text,
-              style: Theme.of(context)
-                  .textTheme
-                  .bodyMedium!
-                  .copyWith(color: Colors.white),
-            ),
+            child: isLoading
+                ? SizedBox(
+                    height: 20,
+                    width: 40,
+                    child: LoadingIndicator(
+                      indicatorType: Indicator.ballPulse,
+                      colors: List.filled(
+                          3, Theme.of(context).colorScheme.onPrimary),
+                    ),
+                  )
+                : Text(
+                    text,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                        color: isUser ? Colors.white : Colors.black87),
+                  ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
-            child: Text(
-              "${date.hour}:${date.minute}",
-              style:
-                  Theme.of(context).textTheme.bodySmall!.copyWith(fontSize: 10),
-            ),
-          )
+          !isLoading
+              ? Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 8, vertical: 2),
+                  child: Text(
+                    "${date.hour}:${date.minute}",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodySmall!
+                        .copyWith(fontSize: 10),
+                  ),
+                )
+              : Container()
         ],
       ),
     );
--- lib/pages/main/ai_bot_page.dart
+++ lib/pages/main/ai_bot_page.dart
@@ -1,14 +1,11 @@
 import 'dart:convert';
-import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:healthsphere/components/chat_ui/chat_bubble.dart';
+import 'package:healthsphere/main.dart';
+import 'package:healthsphere/models/message.dart';
 
-String randomString() {
-  final random = Random.secure();
-  final values = List<int>.generate(16, (i) => random.nextInt(255));
-  return base64UrlEncode(values);
-}
+import 'package:http/http.dart' as http;
 
 class AIBotScreen extends StatefulWidget {
   const AIBotScreen({super.key});
@@ -18,13 +15,124 @@ class AIBotScreen extends StatefulWidget {
 }
 
 class _AIBotScreenState extends State<AIBotScreen> {
+  final TextEditingController promptController = TextEditingController();
+  final ScrollController _controller = ScrollController();
+  List<Message> chats = [];
+  bool isMessageLoading = false;
+  String userId = "";
+  bool loading = true;
+  @override
+  void initState() {
+    getInitMessages();
+    super.initState();
+  }
+
+  void getInitMessages() async {
+    if (supabase.auth.currentUser == null) {
+      setState(() {
+        loading = false;
+      });
+      return;
+    }
+    final res = await supabase.from("messages").select() as List;
+    List<Message> temp = [];
+    for (var ele in res) {
+      temp.add(Message(
+          message: ele["prompt"],
+          isUser: true,
+          date: DateTime.parse(ele["created_at"] ?? "")));
+      temp.add(Message(
+          message: ele["response"],
+          isUser: false,
+          date: DateTime.parse(ele["created_at"] ?? "")));
+    }
+    setState(() {
+      chats.addAll(temp);
+      loading = false;
+    });
+  }
+
+  void sendMessage() async {
+    if (isMessageLoading || loading) {
+      return;
+    }
+    final message = promptController.text.trim();
+    if (message.isEmpty) {
+      return;
+    }
+    setState(() {
+      chats.add(Message(message: message, isUser: true, date: DateTime.now()));
+    });
+    _controller.animateTo(
+      0.0,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.fastOutSlowIn,
+    );
+    promptController.clear();
+    setState(() {
+      isMessageLoading = true;
+      chats.add(Message(message: "", isUser: false, date: DateTime.now()));
+    });
+    _controller.animateTo(
+      0.0,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.fastOutSlowIn,
+    );
+    try {
+      final body =
+          jsonEncode({"model": "mistral", "prompt": message, "stream": false});
+      final res = await http.post(
+        Uri.https("cruel-bushes-report.loca.lt", "api/generate"),
+        headers: {"Content-Type": "application/json"},
+        body: body,
+      );
+      var decodedResponse = jsonDecode(utf8.decode(res.bodyBytes)) as Map;
+      setState(() {
+        chats[chats.length - 1] = Message(
+            message: decodedResponse["response"].toString().trim(),
+            isUser: false,
+            date: DateTime.now());
+        isMessageLoading = false;
+      });
+      _controller.animateTo(
+        0.0,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.fastOutSlowIn,
+      );
+      await supabase.from("messages").insert({
+        "prompt": message,
+        "response": decodedResponse["response"].toString().trim(),
+        "user_id": supabase.auth.currentUser?.id
+      });
+    } catch (e) {
+      if (context.mounted) {
+        setState(() {
+          chats.removeAt(chats.length - 1);
+          isMessageLoading = false;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text(
+            "Something went wrong",
+          ),
+          backgroundColor: Colors.redAccent,
+        ));
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    promptController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("Part A.I"),
+          title: const Text("Partyush A.I"),
           leading: IconButton(
-            icon: Icon(Icons.chevron_left_rounded),
+            icon: const Icon(Icons.chevron_left_rounded),
             onPressed: () {
               Navigator.pop(context);
             },
@@ -32,12 +140,55 @@ class _AIBotScreenState extends State<AIBotScreen> {
         ),
         body: Column(
           children: [
-            Expanded(
-              child: ListView(
+            loading
+                ? const Expanded(
+                    child: Center(child: CircularProgressIndicator()))
+                : Expanded(
+                    child: ListView.builder(
+                        controller: _controller,
+                        itemCount: chats.length,
+                        reverse: true,
+                        itemBuilder: (context, i) {
+                          final index = chats.length - 1 - i;
+                          if (index == chats.length - 1 && isMessageLoading) {
+                            return ChatBubble(
+                              text: chats[index].message,
+                              isUser: chats[index].isUser,
+                              date: chats[index].date,
+                              isLoading: true,
+                            );
+                          }
+                          return ChatBubble(
+                            text: chats[index].message,
+                            isUser: chats[index].isUser,
+                            date: chats[index].date,
+                            isLoading: false,
+                          );
+                        }),
+                  ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
                 children: [
-                  ChatBubble(
-                      text: "What is this", isUser: true, date: DateTime.now()),
-                  ChatBubble(text: "Hello", isUser: false, date: DateTime.now())
+                  Expanded(
+                      child: TextField(
+                    enabled: !isMessageLoading && !loading,
+                    controller: promptController,
+                    decoration:
+                        const InputDecoration(hintText: "Type anything"),
+                  )),
+                  const SizedBox(width: 5),
+                  Container(
+                    padding: const EdgeInsets.all(1.5),
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.primaryContainer,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: IconButton(
+                      onPressed: sendMessage,
+                      icon: const Icon(Icons.send_rounded),
+                    ),
+                  )
                 ],
               ),
             ),
